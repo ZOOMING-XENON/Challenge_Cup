6ff34f457452ad5e7ad70cd7a57c499bd69e579f
yibeitaozhi <1821244709@qq.com>
2025-02-27 15:32:10 +0800

    数据备份功能完成

--- flask正式系统/cloud_config.py
+++ flask正式系统/cloud_config.py
@@ -1,4 +1,21 @@
 # 云存储配置
 CLOUD_STORAGE_CONFIG = {
-
-} 
+    'aliyun': {
+        'access_key_id': '你的阿里云AccessKeyId',
+        'access_key_secret': '你的阿里云AccessKeySecret',
+        'endpoint': 'oss-cn-hangzhou.aliyuncs.com',
+        'bucket_name': '你的Bucket名称'
+    },
+    'qcloud': {
+        'secret_id': 'AKIDKcjjX9aBmCTZWHsHUUN4Zrwrb9XlpjT1',
+        'secret_key': 'RgleNwHQN9j6HKEIOQQL9lYEi0GTduHV',
+        'region': 'ap-guangzhou',
+        'bucket_name': 'mengshupei-1345234371'
+    },
+    'aws': {
+        'aws_access_key_id': '你的AWS访问密钥ID',
+        'aws_secret_access_key': '你的AWS访问密钥',
+        'region_name': 'ap-northeast-1',
+        'bucket_name': '你的Bucket名称'
+    }
+}
