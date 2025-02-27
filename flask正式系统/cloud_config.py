e5251f2e51f484dd350e7286d8846fc7d83d03f1
yibeitaozhi <1821244709@qq.com>
2025-02-27 15:08:46 +0800

    敏感信息调整

--- flask正式系统/cloud_config.py
+++ flask正式系统/cloud_config.py
@@ -1,21 +1,4 @@
 # 云存储配置
 CLOUD_STORAGE_CONFIG = {
-    'aliyun': {
-        'access_key_id': '你的阿里云AccessKeyId',
-        'access_key_secret': '你的阿里云AccessKeySecret',
-        'endpoint': 'oss-cn-hangzhou.aliyuncs.com',
-        'bucket_name': '你的Bucket名称'
-    },
-    'qcloud': {
-        'secret_id': '',
-        'secret_key': '',
-        'region': '',
-        'bucket_name': ''
-    },
-    'aws': {
-        'aws_access_key_id': '你的AWS访问密钥ID',
-        'aws_secret_access_key': '你的AWS访问密钥',
-        'region_name': 'ap-northeast-1',
-        'bucket_name': '你的Bucket名称'
-    }
+
 } 
